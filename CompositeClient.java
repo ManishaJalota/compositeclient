75ee96dffc53624f7cdc04ab96e72da84e4cc861
ManishaJalota <44890868+ManishaJalota@users.noreply.github.com>
2019-03-31 13:13:27 -0400

    Update CompositeClient.java

--- CompositeClient.java
+++ CompositeClient.java
@@ -1,26 +1,17 @@
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java. util.*;
 public class CompositeClient {
 
-	public static void main(String args[]) {
-	ArrayList<String> newlist= new ArrayList<String>();
-	ArrayList<String> newlist2= new ArrayList<String>();
-	ArrayList<String> newlist3= new ArrayList<String>();
-	newlist.add("Wake Up at 6AM");
-	newlist.add("Have a really good breakfast");
-	newlist.add("Go to shower and look good");
-	newlist2.add("Cafe latte");
-	newlist2.add("cafe filter");
-	CompositeInterface ob= new Composite("Morning Routine", newlist);
-     CompositeInterface	ob4=new Composite("Drink a new coffee daily", newlist2);
-	System.out.println(ob.getToDo());
-	System.out.println(ob.multiToDo());
-	System.out.println(ob4.getToDo());
-	System.out.println(ob4.multiToDo());
-	CompositeInterface ob2= new Leaf("Code hard");
+	public static <E> void main(String args[]) 
+	{
 	
-	System.out.println(ob2.getToDo());
-	ob2.toString();
+		ArrayList<String> List= new ArrayList<String>();
+		CompositeInterface leaf1= new Leaf("Morning wakeup");
+		CompositeInterface leaf2= new Leaf("Look good");
+		CompositeInterface Composite1= new Composite("Making Good breakfast");
+		Composite1.addTask();
+		leaf1.addTask();
+		
 	}
 }
