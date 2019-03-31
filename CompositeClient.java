0e48d3a4f819de06ffa47e79aafd3a8bb5c3704e
ManishaJalota <44890868+ManishaJalota@users.noreply.github.com>
2019-03-31 18:43:58 -0400

    Update CompositeClient.java

--- CompositeClient.java
+++ CompositeClient.java
@@ -10,8 +10,20 @@ public class CompositeClient {
 		CompositeInterface leaf1= new Leaf("Morning wakeup");
 		CompositeInterface leaf2= new Leaf("Look good");
 		CompositeInterface Composite1= new Composite("Making Good breakfast");
-		Composite1.addTask();
-		leaf1.addTask();
+		
+		
+		CompositeInterface composite2= new Composite("Going to GYM");
+		CompositeInterface leaf3= new Leaf("warm_Up");
+		CompositeInterface composite3 = new Composite("Do something");
+		CompositeInterface leaf4= new Leaf("Read books");
+		composite2.addTask(leaf3);
+		composite2.printfun();
+		composite3.addTask(leaf4);
+		composite3.printfun();
+		//leaf1.addTask();
+		//composite2.addTask();
+		
+		//List.remove(0);
 		
 	}
 }
